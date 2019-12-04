9ef0e2e3cc95a9b41f5f6a9c71c3e3c1dc3524c7
Bruce MacKenzie <mackenzbb@gmail.com>
2019-12-04 15:18:45 -0500

    Abstracted pool of lead phone numbers in db.py; Moved state classes to state.py

--- db.py
+++ db.py
@@ -1,3 +1,6 @@
+import logging
+
+from state import LeadState
 
 class AgentCollection:
     ''' Mock database collection for a logged in agent and their current leads.
@@ -33,8 +36,52 @@ class AgentCollection:
     def remove_lead(self, lead_phone_number: str):
         AgentCollection._agents[self.agent_id].remove(lead_phone_number)
 
-    def get_number_of_leads(self):
+    def get_number_of_leads(self) -> int:
         ''' Return number of leads currently stored for agent_id.
         Raises KeyError if agent_id is missing
         '''
         return len(AgentCollection._agents[self.agent_id])
+
+class LeadCollection:
+    ''' Mock database collection storing the pool of leads.  Note this is a
+    global resource so all interaction is static; there is no interface for
+    instances.
+    '''
+
+    _leads = {
+        '222-384-6115': LeadState.AVAILABLE,
+        '333-911-0414': LeadState.AVAILABLE,
+        '444-763-9619': LeadState.AVAILABLE,
+        '555-430-7663': LeadState.AVAILABLE,
+        '666-919-5533': LeadState.AVAILABLE,
+        '777-609-4348': LeadState.AVAILABLE
+    }
+
+    @staticmethod
+    def get_lead_phone_number_to_dial() -> str:
+        ''' Finds and returns the first available lead, marking it as QUEUED so
+        no other agent can take it.  Returns None is no leads are currently
+        available.
+        '''
+        for lead, state in LeadCollection._leads.items():
+            if state == LeadState.AVAILABLE:
+                LeadCollection._leads[lead] = LeadState.QUEUED
+                return lead
+        logging.warning('No more available leads')
+        return None
+
+    @staticmethod
+    def get_state(lead: str) -> LeadState:
+        ''' Given a lead phone number return its state, None if no found
+        '''
+        if lead in LeadCollection._leads:
+            return LeadCollection._leads[lead]
+        else:
+            return None
+
+    @staticmethod
+    def update_state(lead: str, state: LeadState):
+        ''' Update lead with state.  No effect if lead not found.
+        '''
+        if lead in LeadCollection._leads:
+            LeadCollection._leads[lead] = state
--- power_dialer.py
+++ power_dialer.py
@@ -1,37 +1,15 @@
 
-import enum
 import logging
 from pprint import pprint
 import random
 
-from db import AgentCollection
+from state import AgentState, LeadState
+from db import AgentCollection, LeadCollection
 
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
 
-class AgentState(enum.Enum):
-    OFFLINE = 1
-    IDLE = 2
-    ENGAGED = 3
-
-class LeadState(enum.Enum):
-    AVAILABLE = 1
-    QUEUED = 2
-    STARTED = 3
-    FAILED = 4
-    ENDED = 5
-    ABANDONED = 6
-
-DIAL_RATIO = 2
-
-lead_phone_numbers_pool = {
-    '222-384-6115': LeadState.AVAILABLE,
-    '333-911-0414': LeadState.AVAILABLE,
-    '444-763-9619': LeadState.AVAILABLE,
-    '555-430-7663': LeadState.AVAILABLE,
-    '666-919-5533': LeadState.AVAILABLE,
-    '777-609-4348': LeadState.AVAILABLE
-}
+DIAL_RATIO = 3
 
 
 class PowerDialer:
@@ -46,7 +24,7 @@ class PowerDialer:
 
         number_of_leads = self.agent_collection.get_number_of_leads()  # should always return zero
         while (number_of_leads) < DIAL_RATIO:
-            lead = get_lead_phone_number_to_dial()
+            lead = LeadCollection.get_lead_phone_number_to_dial()
             if lead:
                 number_of_leads += 1
                 self.agent_collection.add_lead(lead)
@@ -68,10 +46,10 @@ class PowerDialer:
         # Modify state of current calls
         for lead in self.agent_collection.get_leads():
             if lead == lead_phone_number:
-                lead_phone_numbers_pool[lead] = LeadState.ENDED
+                LeadCollection.update_state(lead, LeadState.ENDED)
             else:
-                if lead_phone_numbers_pool[lead] == LeadState.STARTED:
-                    lead_phone_numbers_pool[lead] = LeadState.ABANDONED
+                if LeadCollection.get_state(lead) == LeadState.STARTED:
+                    LeadCollection.update_state(lead, LeadState.ABANDONED)
                 self.agent_collection.remove_lead(lead)
                 # if call failed to nothing
 
@@ -86,7 +64,7 @@ class PowerDialer:
         # Attempt to fill back up
         number_of_leads = self.agent_collection.get_number_of_leads()
         while (number_of_leads < DIAL_RATIO):
-            lead = get_lead_phone_number_to_dial()
+            lead = LeadCollection.get_lead_phone_number_to_dial()
             if lead:
                 self.agent_collection.add_lead(lead)
                 dial(self.agent_id, lead)
@@ -106,7 +84,7 @@ class PowerDialer:
         # Attempt to fill back up
         number_of_leads = self.agent_collection.get_number_of_leads()
         while (number_of_leads < DIAL_RATIO):
-            lead = get_lead_phone_number_to_dial()
+            lead = LeadCollection.get_lead_phone_number_to_dial()
             if lead:
                 self.agent_collection.add_lead(lead)
                 dial(self.agent_id, lead)
@@ -129,19 +107,7 @@ def dial(agent_id: str, lead_phone_number: str):
     code.
     '''
     dial_result = random.choice([LeadState.STARTED, LeadState.FAILED])
-    lead_phone_numbers_pool[lead_phone_number] = dial_result
-
-
-def get_lead_phone_number_to_dial() -> str:
-    ''' Return the phone number of an available lead.  Returns None is no leads
-    are currently available.
-    '''
-    for lead, state in lead_phone_numbers_pool.items():
-        if state == LeadState.AVAILABLE:
-            lead_phone_numbers_pool[lead] = LeadState.QUEUED
-            return lead
-    logging.warning('No more available leads')
-    return None
+    LeadCollection.update_state(lead_phone_number, dial_result)
 
 
 def main():
@@ -153,16 +119,16 @@ def main():
         # Print out current state
         print()
         pprint(dialer.agent_collection.get_leads())
-        pprint(lead_phone_numbers_pool)
+        pprint(LeadCollection._leads)
 
         # Get latest Lead and Agent states
         lead = dialer.agent_collection.get_next_lead()
-        number_state = lead_phone_numbers_pool[lead]
+        lead_state = LeadCollection.get_state(lead)
 
         # State machine
-        if dialer.agent_state == AgentState.IDLE and number_state == LeadState.STARTED:
+        if dialer.agent_state == AgentState.IDLE and lead_state == LeadState.STARTED:
             number_of_leads = dialer.on_call_started(lead)
-        elif dialer.agent_state == AgentState.IDLE and number_state == LeadState.FAILED:
+        elif dialer.agent_state == AgentState.IDLE and lead_state == LeadState.FAILED:
             number_of_leads = dialer.on_call_failed(lead)
         elif dialer.agent_state == AgentState.ENGAGED:
             number_of_leads = dialer.on_call_ended(lead)
@@ -170,7 +136,7 @@ def main():
     dialer.on_agent_logout()
 
     print()
-    pprint(lead_phone_numbers_pool)
+    pprint(LeadCollection._leads)
 
 
 if __name__ == '__main__':
