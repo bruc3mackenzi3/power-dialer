247b8a05951016ee43190d90209674bfeed006d7
Bruce MacKenzie <mackenzbb@gmail.com>
2019-12-04 12:07:16 -0500

    Abstracted storage of agent leads out into stubbed database db.py

---
+++ db.py
@@ -0,0 +1,39 @@
+
+class AgentCollection:
+    ''' Mock database collection for a logged in agent and their current leads.
+    '''
+
+    _agents = {}
+
+    def __init__(self, agent_id: str):
+        self.agent_id = agent_id
+        AgentCollection._agents[self.agent_id] = []
+
+    def remove_agent(self):
+        if self.agent_id in AgentCollection._agents:
+            del AgentCollection._agents[self.agent_id]
+
+    def add_lead(self, lead_phone_number: str):
+        ''' Raises KeyError if agent_id is missing
+        '''
+        AgentCollection._agents[self.agent_id].append(lead_phone_number)
+
+    def get_next_lead(self) -> str:
+        ''' Returns the next lead phone number to engage
+        '''
+        if AgentCollection._agents[self.agent_id]:
+            return AgentCollection._agents[self.agent_id][0]
+        else:
+            return None
+
+    def get_leads(self) -> list:
+        return AgentCollection._agents[self.agent_id]
+
+    def remove_lead(self, lead_phone_number: str):
+        AgentCollection._agents[self.agent_id].remove(lead_phone_number)
+
+    def get_number_of_leads(self):
+        ''' Return number of leads currently stored for agent_id.
+        Raises KeyError if agent_id is missing
+        '''
+        return len(AgentCollection._agents[self.agent_id])
--- power_dialer.py
+++ power_dialer.py
@@ -4,6 +4,8 @@ import logging
 from pprint import pprint
 import random
 
+from db import AgentCollection
+
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
 
@@ -32,66 +34,79 @@ lead_phone_numbers_pool = {
 class PowerDialer:
     def __init__(self, agent_id: str):
         self.agent_id = agent_id
-
         self._alter_agent_state(AgentState.OFFLINE)
-        self.current_leads = None
+
 
     def on_agent_login(self):
         self._alter_agent_state(AgentState.IDLE)
-        self.current_leads = []
+        self.agent_collection = AgentCollection(self.agent_id)
 
-        while (len(self.current_leads) < DIAL_RATIO):
-            if self._get_lead_phone_number_to_dial():
-                dial(self.agent_id, self.current_leads[-1])
+        number_of_leads = self.agent_collection.get_number_of_leads()  # should always return zero
+        while (number_of_leads) < DIAL_RATIO:
+            lead = get_lead_phone_number_to_dial()
+            if lead:
+                number_of_leads += 1
+                self.agent_collection.add_lead(lead)
+                dial(self.agent_id, lead)
             else:
                 break  # out of numbers
 
+        return self.agent_collection.get_number_of_leads()
+
+
     def on_agent_logout(self):
         self._alter_agent_state(AgentState.OFFLINE)
-        self.current_leads = []
+        self.agent_collection.remove_agent()
+
 
     def on_call_started(self, lead_phone_number: str):
         self._alter_agent_state(AgentState.ENGAGED)
 
         # Modify state of current calls
-        for lead in self.current_leads:
+        for lead in self.agent_collection.get_leads():
             if lead == lead_phone_number:
                 lead_phone_numbers_pool[lead] = LeadState.ENDED
             else:
                 if lead_phone_numbers_pool[lead] == LeadState.STARTED:
                     lead_phone_numbers_pool[lead] = LeadState.ABANDONED
-            # if call failed to nothing
+                self.agent_collection.remove_lead(lead)
+                # if call failed to nothing
 
-        # Drop other calls
-        self.current_leads = [lead_phone_number]
+        return self.agent_collection.get_number_of_leads()
+
 
     def on_call_failed(self, lead_phone_number: str):
         # Remove number
-        self.current_leads.remove(lead_phone_number)
+        self.agent_collection.remove_lead(lead_phone_number)
 
         # Attempt to fill back up
-        while (len(self.current_leads) < DIAL_RATIO):
-            if self._get_lead_phone_number_to_dial():
-                dial(self.agent_id, self.current_leads[-1])
+        number_of_leads = self.agent_collection.get_number_of_leads()
+        while (number_of_leads < DIAL_RATIO):
+            lead = get_lead_phone_number_to_dial()
+            if lead:
+                self.agent_collection.add_lead(lead)
+                dial(self.agent_id, lead)
+                number_of_leads += 1
             else:
-                break  # out of numbers
+                break  # the pool is out of numbers
+
+        return self.agent_collection.get_number_of_leads()
+
 
     def on_call_ended(self, lead_phone_number: str):
         ''' Remove number from local state and dial next number
         '''
         self._alter_agent_state(AgentState.IDLE)
-        self.current_leads.remove(lead_phone_number)
+        self.agent_collection.remove_lead(lead_phone_number)
 
-        number = self._get_lead_phone_number_to_dial()
-        if number:
-            dial(self.agent_id, number)
+        # TODO: adapt loop from on_call_failed
+        lead = get_lead_phone_number_to_dial()
+        if lead:
+            self.agent_collection.add_lead(lead)
+            dial(self.agent_id, lead)
 
-    # Private helper functions
-    def _get_lead_phone_number_to_dial(self):
-        number = get_lead_phone_number_to_dial()
-        if number:
-            self.current_leads.append(number)
-        return number
+        return self.agent_collection.get_number_of_leads()
+
 
     def _alter_agent_state(self, state):
         logging.info(str(state))
@@ -104,7 +119,6 @@ def dial(agent_id: str, lead_phone_number: str):
     Once complete set the state in the DB which can be checked by the calling
     code.
     '''
-
     dial_result = random.choice([LeadState.STARTED, LeadState.FAILED])
     lead_phone_numbers_pool[lead_phone_number] = dial_result
 
@@ -113,7 +127,6 @@ def get_lead_phone_number_to_dial() -> str:
     ''' Return the phone number of an available lead.  Returns None is no leads
     are currently available.
     '''
-
     for lead, state in lead_phone_numbers_pool.items():
         if state == LeadState.AVAILABLE:
             lead_phone_numbers_pool[lead] = LeadState.QUEUED
@@ -124,26 +137,26 @@ def get_lead_phone_number_to_dial() -> str:
 
 def main():
     dialer = PowerDialer('agent1')
-    dialer.on_agent_login()
+    number_of_leads = dialer.on_agent_login()
 
     # main event loop
-    while (len(dialer.current_leads) > 0):
+    while number_of_leads > 0:
         # Print out current state
         print()
-        pprint(dialer.current_leads)
+        pprint(dialer.agent_collection.get_leads())
         pprint(lead_phone_numbers_pool)
 
         # Get latest Lead and Agent states
-        number = dialer.current_leads[0]
-        number_state = lead_phone_numbers_pool[number]
+        lead = dialer.agent_collection.get_next_lead()
+        number_state = lead_phone_numbers_pool[lead]
 
         # State machine
         if dialer.agent_state == AgentState.IDLE and number_state == LeadState.STARTED:
-            dialer.on_call_started(number)
+            number_of_leads = dialer.on_call_started(lead)
         elif dialer.agent_state == AgentState.IDLE and number_state == LeadState.FAILED:
-            dialer.on_call_failed(number)
+            number_of_leads = dialer.on_call_failed(lead)
         elif dialer.agent_state == AgentState.ENGAGED:
-            dialer.on_call_ended(number)
+            number_of_leads = dialer.on_call_ended(lead)
 
     dialer.on_agent_logout()
 
